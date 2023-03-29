4a39ff5a6d2174736f805f1a2254d9c7694fad2d
AlexAP68 <alexalameda@paucasesnovescifp.cat>
2023-03-29 22:09:30 +0200

    Alex prueba

--- Comprobar.java
+++ Comprobar.java
@@ -1,3 +1,16 @@
 public class Comprobar{
     
+    public static boolean compruebaPassword(String password) {
+        boolean valido = false;
+
+        // Comprobar que la contraseña cumple las restricciones
+        if (password.matches("^[A-Z][a-zA-Z0-9_#@-]{6}[0-9]{2}$")) {
+        valido = true;
+        } else {
+        System.out.println("La contraseña no cumple las restricciones");
+        }
+
+        return valido;
+        }
+// Método para comprobar la contraseña
 }
