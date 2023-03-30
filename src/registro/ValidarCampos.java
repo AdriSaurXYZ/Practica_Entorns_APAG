ec9cd26c8918cf4fc86edf12d1827ed3d3a20c08
AdriSaurXYZ <adrianaguilo@paucasesnovescifp.cat>
2023-03-30 23:00:09 +0200

    Comentario Validar

--- src/registro/ValidarCampos.java
+++ src/registro/ValidarCampos.java
@@ -1,31 +1,42 @@
+/**
+
+Clase que permite validar el formulario de registro de usuario
+*/
 package registro;
-
 public class ValidarCampos {
-    public boolean validarFormulario(RegistroUsuario registroUsuario) {
-        
-       // Checking if the name is valid.
-        if (!Comprobar.compruebaNombre(Comprobar.nombresUsuarios, registroUsuario.getNombre())) {
-            return false;
-        }
-        
 
-      // Checking if the email is valid.
-        if (!Comprobar.compruebaEmail(registroUsuario.getEmail())) {
-            return false;
-        }
-
-        // Checking if the password is valid.
-        if (!Comprobar.compruebaPassword(registroUsuario.getPassword())) {
-            return false;
-        }
-
-      // Checking if the security code is valid.
-        if (!Comprobar.compruebaCodigoSeguridad(registroUsuario.getCodigo())) {
-            return false;
-        }
-        
-        
-
-        return true;
+kotlin
+Copy code
+/**
+ * Método que valida el formulario de registro de usuario
+ * @param registroUsuario objeto de tipo RegistroUsuario con los datos del usuario
+ * @return true si el formulario es válido, false en caso contrario
+ */
+public boolean validarFormulario(RegistroUsuario registroUsuario) {
+    
+   // Verifica si el nombre es válido.
+    if (!Comprobar.compruebaNombre(Comprobar.nombresUsuarios, registroUsuario.getNombre())) {
+        return false;
     }
+    
+
+  // Verifica si el email es válido.
+    if (!Comprobar.compruebaEmail(registroUsuario.getEmail())) {
+        return false;
+    }
+
+    // Verifica si la contraseña es válida.
+    if (!Comprobar.compruebaPassword(registroUsuario.getPassword())) {
+        return false;
+    }
+
+  // Verifica si el código de seguridad es válido.
+    if (!Comprobar.compruebaCodigoSeguridad(registroUsuario.getCodigo())) {
+        return false;
+    }
+    
+    
+
+    return true;
+}
 }
