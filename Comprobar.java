e63fa7f31701e9b3bde2a1d2ddcd4ffc391e0138
AlexAP68 <alexalameda@paucasesnovescifp.cat>
2023-03-30 11:36:53 +0200

    finalizando rama 1

--- Comprobar.java
+++ Comprobar.java
@@ -1,10 +1,12 @@
 public class Comprobar{
     
+
+    // Método para comprobar la contraseña
     public static boolean compruebaPassword(String password) {
         boolean valido = false;
 
         // Comprobar que la contraseña cumple las restricciones
-        if (password.matches("^[A-Z][a-zA-Z0-9_#@-]{6}[0-9]{2}$")) {
+        if (password.matches("^[A-Z](?=.*[@#_-])[A-Za-z0-9@#_-]{5}[0-9]{2}$")) {
         valido = true;
         } else {
         System.out.println("La contraseña no cumple las restricciones");
@@ -12,5 +14,5 @@ public class Comprobar{
 
         return valido;
         }
-// Método para comprobar la contraseña
+
 }
